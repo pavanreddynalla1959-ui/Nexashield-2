2060fff488dea8897a89d37eab3cd4b6dc86f9c6
pavanreddynalla1959-ui <pavanreddynalla1959@gmail.com>
2026-08-09 09:52:59 +0530

    Update cij-triggers.js

--- k/js/cij-triggers.js
+++ k/js/cij-triggers.js
@@ -36,7 +36,7 @@ window.CIJTriggers = {
 
         window.msdynmkt.trackEvent({
             name: "msdynmkt_webinarattended_032822006",
-            ingestionKey: "",
+            ingestionKey: "https://org4699256e.crm.dynamics.com/main.aspx?appid=11fd5cd2-9786-f111-ab0f-000d3a59d82d&pagetype=entityrecord&etn=msdynmkt_eventmetadata&id=baffb515-a393-f111-8075-000d3a59d82d&formid=1d9e097d-ee10-455c-8d69-507df1d49101",
             version: "1.0.0",
 
             properties: {
